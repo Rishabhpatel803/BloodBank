281c7f6fee81f849671fa1aa85e80791f097a865
Rishabh Patel <56493772+Rishabhpatel803@users.noreply.github.com>
2021-07-28 20:56:40 +0530

    Security fix

--- src/main/java/LoginDao.java
+++ src/main/java/LoginDao.java
@@ -6,7 +6,7 @@ public class LoginDao {
         Connection connection=null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank","lucious","u*r*good1");
+            connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/bloodbank","YOUR_SQL_NAME","YOUR_PASSWORD");
             return connection;
         }catch(Exception e){System.out.println(e);}
         return connection;
